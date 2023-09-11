927f6d6ec372235a74d5fd43d584225324643822
Andrew D Smith <andrewds@usc.edu>
2023-09-11 15:13:20 -0700

    Adding logger classes

--- src/common/dnmt_logger.hpp
+++ src/common/dnmt_logger.hpp
@@ -16,21 +16,28 @@
 #ifndef DNMT_LOGGER_HPP
 #define DNMT_LOGGER_HPP
 
-#include <string>
-#include <ostream>
 #include <chrono>
+#include <ostream>
+#include <string>
 
 struct dnmt_logger {
   std::ostream &log_stream;
   std::string prefix;
-  dnmt_logger(std::ostream &ls, std::string &&p) log_stream{ls}, prefix{} {}
-  dnmt_logger(std::ostream &ls, std::string &&p) log_stream{ls}, prefix{p} {}
-  std::chrono::steady_clock log_event(std::string message);
-  std::chrono::steady_clock log_event(std::string message,
-                                      std::chrono::steady_clock sclk);
-  std::chrono::steady_clock log_data(std::string key, std::string value);
-  std::chrono::steady_clock log_data(std::string key, std::string value,
-                                     std::chrono::steady_clock sclk);
+
+  dnmt_logger(std::ostream &ls): log_stream{ls}, prefix{} {}
+
+  dnmt_logger(std::ostream &ls, std::string &&p): log_stream{ls}, prefix{p} {}
+
+  std::chrono::time_point<std::chrono::steady_clock>
+  log_event(std::string message);
+  std::chrono::time_point<std::chrono::steady_clock>
+  log_event(std::string message,
+            std::chrono::time_point<std::chrono::steady_clock> sclk);
+  std::chrono::time_point<std::chrono::steady_clock>
+  log_data(std::string key, std::string value);
+  std::chrono::time_point<std::chrono::steady_clock>
+  log_data(std::string key, std::string value,
+           std::chrono::time_point<std::chrono::steady_clock> sclk);
 };
 
 #endif
